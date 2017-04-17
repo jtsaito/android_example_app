410bdff001ce9b2eb36f8b366a0be2753511c1ca
jtsaito <jahn.saito@gmail.com>
2017-04-17 14:42:00 +0200

    Get photo and save to tempfile

--- app/src/main/java/com/example/jsaito/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jsaito/myapplication/MainActivity.java
@@ -1,10 +1,30 @@
 package com.example.jsaito.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -17,10 +37,225 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
+     /*
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+        */
+
+        dispatchTakePictureIntent();
     }
+
+
+    // take picture
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+    // create file
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        Log.v("TAG", "path=" + storageDir);
+
+        Log.v("JTS", "FileName=" + imageFileName);
+
+        File image = null;
+
+        try {
+             image = File.createTempFile(
+                    imageFileName,
+                    ".jpg",
+                    storageDir
+            );
+        } catch( Exception e) {
+            Log.v("JTS", e.toString());
+        }
+
+
+        Log.v("JTS", "done");
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+
+        Log.v("JTS", "path=" + mCurrentPhotoPath);
+
+        return image;
+    }
+
+
+    /*
+    // upload file
+    public void UploadFile(){
+        try {
+            // Set your file path here
+            FileInputStream fstrm = new FileInputStream(Environment.getExternalStorageDirectory().toString()+"/DCIM/file.mp4");
+
+            // Set your server page url (and the file title/description)
+            HttpFileUpload hfu = new HttpFileUpload("http://34.253.87.41/upload", "Title","Description");
+
+            hfu.Send_Now(fstrm);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public class HttpFileUpload implements Runnable{
+        URL connectURL;
+        String responseString;
+        String Title;
+        String Description;
+        byte[ ] dataToServer;
+        FileInputStream fileInputStream = null;
+
+        HttpFileUpload(String urlString, String vTitle, String vDesc){
+            try{
+                connectURL = new URL(urlString);
+                Title= vTitle;
+                Description = vDesc;
+            }catch(Exception ex){
+                Log.i("HttpFileUpload","URL Malformatted");
+            }
+        }
+
+        void Send_Now(FileInputStream fStream){
+            fileInputStream = fStream;
+            Sending();
+        }
+
+        void Sending(){
+            String iFileName = "ovicam_temp_vid.mp4";
+            String lineEnd = "\r\n";
+            String twoHyphens = "--";
+            String boundary = "*****";
+            String Tag="fSnd";
+            try
+            {
+                Log.e(Tag,"Starting Http File Sending to URL");
+
+                // Open a HTTP connection to the URL
+                HttpURLConnection conn = (HttpURLConnection)connectURL.openConnection();
+
+                // Allow Inputs
+                conn.setDoInput(true);
+
+                // Allow Outputs
+                conn.setDoOutput(true);
+
+                // Don't use a cached copy.
+                conn.setUseCaches(false);
+
+                // Use a post method.
+                conn.setRequestMethod("POST");
+
+                conn.setRequestProperty("Connection", "Keep-Alive");
+
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
+
+                DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"title\""+ lineEnd);
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(Title);
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+
+                dos.writeBytes("Content-Disposition: form-data; name=\"description\""+ lineEnd);
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(Description);
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + iFileName +"\"" + lineEnd);
+                dos.writeBytes(lineEnd);
+
+                Log.e(Tag,"Headers are written");
+
+                // create a buffer of maximum size
+                int bytesAvailable = fileInputStream.available();
+
+                int maxBufferSize = 1024;
+                int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                byte[ ] buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                int bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0)
+                {
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable,maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0,bufferSize);
+                }
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // close streams
+                fileInputStream.close();
+
+                dos.flush();
+
+                Log.e(Tag,"File Sent, Response: "+String.valueOf(conn.getResponseCode()));
+
+                InputStream is = conn.getInputStream();
+
+                // retrieve the response from server
+                int ch;
+
+                StringBuffer b =new StringBuffer();
+                while( ( ch = is.read() ) != -1 ){ b.append( (char)ch ); }
+                String s=b.toString();
+                Log.i("Response",s);
+                dos.close();
+            }
+            catch (MalformedURLException ex)
+            {
+                Log.e(Tag, "URL error: " + ex.getMessage(), ex);
+            }
+
+            catch (IOException ioe)
+            {
+                Log.e(Tag, "IO error: " + ioe.getMessage(), ioe);
+            }
+        }
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+        }
+    }
+    */
+
 }
